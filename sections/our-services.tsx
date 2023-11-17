826d13f9be1967cf3b9f857b2b4ccea77581ae57
KeithScheldt <keithscheldt@gmail.com>
2023-11-17 15:20:26 -0500

    Refactor our-services.tsx to improve code formatting

--- sections/our-services.tsx
+++ sections/our-services.tsx
@@ -7,8 +7,11 @@ const Services = () => {
   return (
     <section id='services'>
       <Container>
-        <h1 className='flex justify-center text-3xl font-semibold pb-10'>Services</h1>
-        <div className='flex flex-row justify-center gap-9'>
+        <h1 className='flex justify-center text-3xl font-semibold pb-10'>
+          Services
+          </h1>
+
+        <div className='flex flex-row justify-center gap-9 mx-10'>
         {services.map((service) => (
           <ServiceCard
             key={service.label}
